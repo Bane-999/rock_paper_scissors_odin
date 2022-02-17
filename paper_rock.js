1664db4c5060d0fd7c862a2d19b706ed860a75a0
Bane-999 <bane-93erep@hotmail.com>
2022-02-17 01:04:49 +0100

    add 5 rounds and score

--- paper_rock.js
+++ paper_rock.js
@@ -17,50 +17,77 @@ function computerPlay() {
     return comp_pick;
 }
 
-function playRound(x, y) {
-    console.log(`Computer picks: ${y}`);
-    if(x == "rock") {
-        if(y == "rock") {
-            return "It's a Draw!";
+function playRound(y) {
+    let user_pick = prompt("Choose rock, paper or scissors.");
+    user_pick = user_pick.toLowerCase();
+    if(user_pick == "rock" || user_pick == "paper" || user_pick == "scissors") {
+
+        console.log(`Computer picks: ${y}`);
+        if(user_pick == "rock") {
+            if(y == "rock") {
+                return "It's a Draw!";
+            }
+            else if(y == "paper") {
+                return "You lose!";
+            }
+            else {
+                return "You won!";
+            }
         }
-        else if(y == "paper") {
-            return "You lose!";
+        else if(user_pick == "paper") {
+            if(y == "rock") {
+                return "You won!";
+            }
+            else if(y == "paper") {
+                return "It's a Draw!";
+            }
+            else {
+                return "You lose!";
+            }
         }
         else {
-            return "You won!";
+            if(y == "rock") {
+                return "You lose!";
+            }
+            else if(y == "paper") {
+                return "You won!";
+            }
+            else {
+                return "It's a Draw!";
+            }
         }
+    }  
+    else {
+        console.log("Wrong input!");
+    }  
+}
+
+function game() {
+    let score_user=0, score_comp=0;
+    let score;
+    for(let i=0; i<5; i++) {
+        score = playRound(computerPlay());
+        if(score == "It's a Draw!") {
+            score_user++;
+            score_comp++;
+        }
+        else if(score == "You lose!") {
+            score_comp++;
+        }
+        else {
+            score_user++;
+        }
+        console.log(score);
     }
-    else if(x == "paper") {
-        if(y == "rock") {
-            return "You won!";
-        }
-        else if(y == "paper") {
-            return "It's a Draw!";
-        }
-        else {
-            return "You lose!";
-        }
+    if(score_user > score_comp) {
+        console.log(`Congratulations. You have won the game! Final score is: ${score_user}/${score_comp}`);
+    }
+    else if(score_user == score_comp) {
+        console.log(`It's a Draw! Final score is: ${score_user}/${score_comp}`);
     }
     else {
-        if(y == "rock") {
-            return "You lose!";
-        }
-        else if(y == "paper") {
-            return "You won!";
-        }
-        else {
-            return "It's a Draw!";
-        }
+        console.log(`You lose the game! Final score is: ${score_user}/${score_comp}`);
     }
 }
 
-let user_pick = prompt("Choose rock, paper or scissors.");
-user_pick = user_pick.toLowerCase();
-if(user_pick == "rock" || user_pick == "paper" || user_pick == "scissors") {
-    console.log(playRound(user_pick, computerPlay()));
-}
-else {
-    console.log("Wrong input!");
-}
-
-//console.log(computerPlay());
+game();
